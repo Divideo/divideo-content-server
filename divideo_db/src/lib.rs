75c63efbcf474c95a8d203d70191f9f670fb1309
lthoerner <lowellashton@gmail.com>
2023-07-26 12:58:05 -0700

    feat(model): add basic data models for database querying

--- divideo_db/src/lib.rs
+++ divideo_db/src/lib.rs
@@ -1,12 +1,11 @@
 use std::path::PathBuf;
 
-use divideo_models::VideoDescriptor;
+use divideo_models::{SearchQuery, VideoDescriptor};
 
 pub trait Database
 where
     Self: Sized,
 {
-    fn open(path: PathBuf) -> Option<Self>;
-
-    fn search(&mut self, start: usize, num: usize) -> Vec<VideoDescriptor>;
+    fn open(path: impl Into<PathBuf>) -> Option<Self>;
+    fn search(&mut self, query: SearchQuery, start: usize, num: usize) -> Vec<VideoDescriptor>;
 }
--- divideo_models/src/lib.rs
+++ divideo_models/src/lib.rs
@@ -1,5 +1,27 @@
+#![allow(dead_code)]
+
+use std::collections::HashMap;
+
+use image::DynamicImage;
+
+/// Represents a video in the database, allowing the client to render video metadata,
+/// then request the video to be streamed from the server.
 pub struct VideoDescriptor {
     pub id: u128,
     pub title: String,
+    // TODO: This should be a Markdown file
     pub description: String,
+    pub thumbnail: DynamicImage,
+    // TODO: Comments
+}
+
+/// Represents all data needed by the content/indexing server to serve search results to the client.
+pub struct SearchQuery {
+    tags: Tags,
+    query: String,
+}
+
+/// Represents tags and their respective weights, used to rank search results.
+pub struct Tags {
+    pub tags: HashMap<String, f32>,
 }
